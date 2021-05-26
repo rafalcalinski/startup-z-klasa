105a2ddafdc78ccd2f095df1ff3c9831826849f2
RafalCalinski <kakti.gamer@gmail.com>
2021-05-26 20:33:45 +0200

    Życie nie ma sensu i wszyscy kiedyś umrzemy :flushed:

---
+++ Coma/Assets/Skrypty/CharacterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    public float speed;
+    Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 mousePos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
