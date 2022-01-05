0c8dfb11081f359ed0eb8380f4ae3fa3034fd4bb
RafalCalinski <kakti.gamer@gmail.com>
2022-01-05 23:34:00 +0100

    Godot ⚙

---
+++ Gra/Player/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+
+const maxSpeed = 80
+const acceleration = 600
+const friction = 500
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * maxSpeed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		
+	velocity = move_and_slide(velocity)
